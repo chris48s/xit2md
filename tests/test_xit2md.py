b1d0733739ce652222367d60796f5d199bc30dbc
chris48s <chris.shaw480@gmail.com>
2022-11-21 17:09:41 +0000

    naive implementation

--- tests/test_xit2md.py
+++ tests/test_xit2md.py
@@ -1,1 +1,37 @@
-# TODO: write some tests
+from xit2md import xit2md_text
+
+input_ = """[ ] Open
+[x] Checked
+[@] Ongoing
+[~] Obsolete
+
+Named Group
+[ ] This #item #has=tags
+[ ] Do this soon -> 2022-01-31
+[ ] !! This is important
+[ ] ! All #the=things -> 2022-12
+
+[ ] Item 1
+[ ] Item 2
+"""
+
+expected = """[//]: # (unnamed group)
+- [ ] Open
+- [x] Checked
+- [ ] Ongoing
+- [x] ~Obsolete~
+
+# Named Group
+- [ ] This #item #has=tags
+- [ ] Do this soon -> 2022-01-31
+- [ ] !! This is important
+- [ ] ! All #the=things -> 2022-12
+
+[//]: # (unnamed group)
+- [ ] Item 1
+- [ ] Item 2
+"""
+
+
+def test():
+    assert xit2md_text(input_) == expected
--- xit2md/__init__.py
+++ xit2md/__init__.py
@@ -1,1 +1,21 @@
-# TODO: write some code
+from stage_left import parse_text
+from stage_left.types import State
+
+
+def xit2md_text(text):
+    out = []
+    parsed = parse_text(text)
+    for group in parsed:
+        if group.title:
+            out.append("# " + group.title)
+        else:
+            out.append("[//]: # (unnamed group)")
+        for item in group.items:
+            if item.state in [State.OPEN, State.ONGOING]:
+                out.append("- [ ] " + item.description)
+            if item.state == State.CHECKED:
+                out.append("- [x] " + item.description)
+            if item.state == State.OBSOLETE:
+                out.append("- [x] ~" + item.description + "~")
+        out.append("")
+    return "\n".join(out)
